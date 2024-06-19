6bbef94325859ff949c83dca1eb873af4e1629cc
Wesley Laura <wrlaurac@unjbg.edu.pe>
2024-06-19 12:03:31 -0500

    Actualizando ejercicio7 de estructuras repetitias: Añadiendo 2 validaciones de entrada,añadiendo una librería para salida de carácteres especiales

--- EJERCICIO7.cpp
+++ EJERCICIO7.cpp
@@ -1,14 +1,29 @@
 /* Diseñar un algoritmo que permita leer “n” números por el teclado. El programa debe
 calcular el promedio de los números pares y el promedio de los números impares */
 #include <iostream>
+#include <windows.h>
 using namespace std;
 int main(){
 	// n:cantidad de numeros a ingresar
+	SetConsoleOutputCP(CP_UTF8);
 	int n, contp=0, conti=0, sumap=0, sumai=0,num,promp,promi;
-	cout<<"ingrese la cantidad de numeros: "; cin>>n;
-	if (n>0){
+	cout<<"INGRESE LA CANTIDAD DE NÚMEROS: "; cin>>n;
+	while (n<0){
+
+		cout<<"ERROR\nINGRESE UN NÚMERO POSITIVO"<<endl;
+		cin>>n;
+		}
 		for ( int i=1;i<=n;i=i+1){
-		cout<<"ingrese el numero: "; cin>>num;
+			
+				cout<<"INGRESE EL NÚMERO "<<i<<":"<<endl;
+		
+		 cin>>num;
+
+		while (num<0) {
+			cout<<"ESCRIBA UN NÚMERO POSITIVO"<<endl;
+		cin>>num;
+		
+		}
 		if (num%2==0){
 			contp++;
 			sumap=sumap+num;
@@ -17,14 +32,12 @@ int main(){
 			sumai=sumai+num;
 		}
 	}
-	}else{
-		cout<<"ERROR"<<endl;
-		return 0;
-	}
+	
+
 	system("cls");
 	promp=sumap/contp;
 	promi=sumai/conti;
-	cout<<"el promedio de numeros pares es: "<<promp<<endl;
-	cout<<"el promedio de numeros impares es: "<<promi<<endl;
+	cout<<"El promedio de números pares es: "<<promp<<endl;
+	cout<<"El promedio de números impares es: "<<promi<<endl;
 	return 0;
 }
